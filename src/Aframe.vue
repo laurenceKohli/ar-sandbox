23df8914d0b1549593523d7b438b909ab68459a0
laurenceKohli <laurence.kohli@hes-so.com>
2025-07-24 13:58:19 +0200

    AR de hiro

---
+++ src/Aframe.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import { ref } from 'vue';
+  import TheScene from './components/TheScene.vue';
+
+  const scale = ref(1);
+  const loaded = ref(true);
+</script>
+
+<template>
+  <TheScene
+    @loaded="loaded = true"
+  />
+</template>
---
+++ src/components/TheScene.vue
@@ -0,0 +1,37 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const allAssetsLoaded = ref(false);
+
+</script>
+
+<template>
+  
+  <a-scene
+    embedded arjs
+  >
+    
+    <a-assets @loaded="allAssetsLoaded = true">
+    </a-assets>
+
+    <template v-if="allAssetsLoaded">
+    <a-marker preset='hiro'>
+      <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+		</a-marker>
+
+    <a-marker-camera >
+      <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+		</a-marker-camera>
+    <!--   <a-box color="red"></a-box>
+
+      
+    <a-box color="green"
+      id="grille-2"
+      position="-0.56 0 0"
+      scale="2 0.1 2"
+      ></a-box> -->
+      
+    </template>
+
+  </a-scene>
+</template>
